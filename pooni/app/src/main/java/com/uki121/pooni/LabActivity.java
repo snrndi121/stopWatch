5a0636ac1b5b6c38bda03b498591357a3584c609
snrndi121 <snrndi4521@gmail.com>
2018-04-05 18:19:31 +0900

    feat Lottie_design on loading page

--- pooni/app/src/main/java/com/uki121/pooni/LabActivity.java
+++ pooni/app/src/main/java/com/uki121/pooni/LabActivity.java
@@ -15,6 +15,7 @@ import android.text.style.StyleSpan;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 /**
--- pooni/app/src/main/java/com/uki121/pooni/MainActivity.java
+++ pooni/app/src/main/java/com/uki121/pooni/MainActivity.java
@@ -8,6 +8,11 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.JsonReader;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+import java.io.StringReader;
 
 public class MainActivity extends AppCompatActivity {
   private static int WELCOME_TIMEOUT = 2000;
@@ -16,6 +21,12 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    /* to do : remove it
+    LottieAnimationView animationView = findViewById(R.id.animation_view);
+    animationView.setAnimation(R.raw.main_clock);
+    animationView.playAnimation();
+    */
 
     new Handler().postDelayed(new Runnable() {
       @Override
