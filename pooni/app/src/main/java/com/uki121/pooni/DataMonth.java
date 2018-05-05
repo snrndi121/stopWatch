6ba5e59c7373294d94ae203f29784a7faf0194d4
snrndi121 <snrndi4521@gmail.com>
2018-05-05 23:04:18 +0900

    class : new class on developing

--- pooni/app/src/main/java/com/uki121/pooni/DataMonth.java
+++ pooni/app/src/main/java/com/uki121/pooni/DataMonth.java
@@ -4,7 +4,14 @@ public class DataMonth {
     //var
     private int numOfmonth;
     private int totalAmount;
-    private float avgAcc;
-    //constructor
+    private float[] accMonth;
+    private float[] accAvg;
+    //constructort
     public DataMonth() {}
+    //set
+    //get
+    public float[] getAverage() { return this.accAvg;}
+    public float[] getAccOfmonth() { return this.accMonth;}
+    public int getTotalAmount() { return this.totalAmount;}
+    public int getNumOfmonth() { return this.numOfmonth;}
 }
